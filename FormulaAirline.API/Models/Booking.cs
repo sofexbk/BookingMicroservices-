62bc6607f06b932d78094d122b3985f66569b4f2
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2024-02-24 14:45:43 +0100

    st

---
+++ FormulaAirline.API/Models/Booking.cs
@@ -0,0 +1,12 @@
+namespace FormulaAirline.API.Models;
+
+public class Booking   
+{
+    public int Id {get;set;}
+    public string PassengerName {get;set;}="";
+    public string PassportNb {get;set;}="";
+    public string From {get;set;}="";
+    public string To {get;set;}="";
+    public int Status {get;set;}
+
+}
